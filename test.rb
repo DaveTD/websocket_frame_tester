7df687354bc95aab35cb09fb343c38965c3aa640
DaveTD <its.dave.dawson@gmail.com>
2013-11-07 21:52:32 -0500

    first commit

---
+++ test.rb
@@ -0,0 +1,10 @@
+require "websocket"
+
+data = "hello"
+frame = WebSocket::Frame::Outgoing::Server.new(:version => 13, :data => data, :type => :text)
+p frame.to_s
+
+data = [145, 133, 67, 114]
+packed = data.pack("cccc")
+frame = WebSocket::Frame::Outgoing::Server.new(:version => 13, :data => packed, :type => :binary)
+p frame.to_s
